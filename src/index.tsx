38d9b344aca18d569823427f69d8796975385926
KushNee <wxtz521@gmail.com>
2017-05-17 22:40:47 +0800

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import Hello from './components/Hello';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Hello name="TypeScript" enthusiasmLevel={10} />,
   document.getElementById('root') as HTMLElement
 );
